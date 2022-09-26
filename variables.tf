dc51b7d168c90f5eb560eeb554af07bd7c8be510
aa-ag <aaron.aguerrevere@gmail.com>
2022-09-25 21:34:37 -0500

    Add example descriptions

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "network_interface_id" {
-    type    = string
-    default = "network_id_from_aws"
+    description = "example description 123"
+    type        = string
+    default     = "network_id_from_aws"
 }
 
 variable "ami" {
+    description = "example description 456"
     type    = string
     default = var.default_ami
 }
